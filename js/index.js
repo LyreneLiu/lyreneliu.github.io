f47f22164501f8e2c3be67d3aa1ed466e3c5aeeb
lyreneliu <lyreneliu@gmail.com>
2020-06-18 21:17:34 +0800

    js test

---
+++ js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+
+	var vm = new Vue({
+		el: "#index",
+		data: function () {
+			return {
+                testText: "hello"
+            };
+		},
+		mounted: function () {
+		},
+		watch: {
+		},
+		methods: {},
+	});
+
+	var that = vm;
+	
+});
